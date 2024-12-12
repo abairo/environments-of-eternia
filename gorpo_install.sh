f88ec5b8444a5243a85834022c411a0e54b45cf1
Anderson Bairo <anderson.bairo@yahoo.com.br>
2024-12-11 21:41:44 -0300

    WIP: add debian support and surfshark vpn

--- gorpo_install.sh
+++ gorpo_install.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-sh ./post_install.sh
+os="$1"
+os=${os,,}
+
+if [[ "$os" == "debian" ]]; then
+    echo "Installing Debian packages..."
+    source ./debian.sh
+    source ./ubuntu.sh
+elif [[ "$os" == "ubuntu" ]]; then
+    echo "Installing Ubuntu packages..."
+    source ./ubuntu.sh
+else
+    echo "Unknown OS..."
+fi
+echo "End of installation"
